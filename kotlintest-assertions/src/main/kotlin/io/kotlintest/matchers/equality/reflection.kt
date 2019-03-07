9dc1e0885a1bb9349ec5aeffbb1e36a5eb4185cb
sksamuel <sam@sksamuel.com>
2019-03-07 08:31:05 +0000

    shouldBeEqualToUsingFields matcher #711

--- kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/equality/reflection.kt
+++ kotlintest-assertions/src/main/kotlin/io/kotlintest/matchers/equality/reflection.kt
@@ -3,12 +3,55 @@ package io.kotlintest.matchers.equality
 import io.kotlintest.Matcher
 import io.kotlintest.Result
 import io.kotlintest.should
-import java.time.LocalDate
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.memberProperties
 
-fun <T> T.shouldBeEqualToIgnoringGivenFields(other: T,
-                                             vararg fields: String) = this should beEqualToIgnoringGivenFields(date)
+fun <T> T.shouldBeEqualToUsingFields(other: T, vararg properties: KProperty<*>) =
+    this should beEqualToUsingFields(other, *properties)
 
-fun haveSameYear(date: LocalDate): Matcher<LocalDate> = object : Matcher<LocalDate> {
-  override fun test(value: LocalDate): Result =
-      Result(value.year == date.year, "$value should have year ${date.year}", "$value should not have year ${date.year}")
+fun <T> beEqualToUsingFields(other: T, vararg fields: KProperty<*>): Matcher<T> = object : Matcher<T> {
+  override fun test(value: T): Result {
+
+    val failed = fields.mapNotNull {
+      val actual = it.getter.call(value)
+      val expected = it.getter.call(other)
+      if (actual == expected) null else {
+        "${it.name}: $actual != $expected"
+      }
+    }
+
+    val fieldsString = fields.joinToString(", ", "[", "]") { it.name }
+
+    return Result(
+        failed.isEmpty(),
+        "$value should be equal to $other using fields $fieldsString; Failed for $failed",
+        "$value should not be equal to $other using fields $fieldsString"
+    )
+  }
+}
+
+fun <T : Any> T.shouldBeEqualToIgnoringFields(other: T, vararg properties: KProperty<*>) =
+    this should beEqualToIgnoringFields(other, *properties)
+
+fun <T : Any> beEqualToIgnoringFields(other: T,
+                                      vararg fields: KProperty<*>): Matcher<T> = object : Matcher<T> {
+  override fun test(value: T): Result {
+
+    val fieldNames = fields.map { it.name }
+    val failed = value::class.memberProperties.filterNot { fieldNames.contains(it.name) }.mapNotNull {
+      val actual = it.getter.call(value)
+      val expected = it.getter.call(other)
+      if (actual == expected) null else {
+        "${it.name}: $actual != $expected"
+      }
+    }
+
+    val fieldsString = fields.joinToString(", ", "[", "]") { it.name }
+
+    return Result(
+        failed.isEmpty(),
+        "$value should be equal to $other ignoring fields $fieldsString; Failed for $failed",
+        "$value should not be equal to $other ignoring fields $fieldsString"
+    )
+  }
 }
